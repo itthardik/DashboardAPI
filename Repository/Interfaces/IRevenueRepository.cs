eef871014af364da0ed8958987c4e8dee54e89c1
itthardik <hardik.sharma@intimetec.com>
2024-09-17 10:59:47 +0530

    Complete Revenue API implementation

---
+++ Repository/Interfaces/IRevenueRepository.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dashboard.Repository.Interfaces
+{
+    /// <summary>
+    /// Revenue Repo Interface
+    /// </summary>
+    public interface IRevenueRepository
+    {
+        //void GetRevenueAndProfitByCategoryAndDate();
+
+        /// <summary>
+        /// Get Product by product id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        JsonResult GetProductCostById(int id);
+
+        /// <summary>
+        /// Get Product by product name
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        JsonResult GetProductCostByName(string name);
+
+        /// <summary>
+        /// Get Top search value by page and page size
+        /// </summary>
+        /// <param name="pageNumber"></param>
+        /// <param name="pageSize"></param>
+        /// <returns></returns>
+        JsonResult GetAllSearchValuesByPagination(int pageNumber, int pageSize);
+        /// <summary>
+        /// Get Revenue Stats Based On Days
+        /// </summary>
+        /// <param name="days"></param>
+        /// <returns></returns>
+        JsonResult GetRevenueStatsBasedOnDays(int days);
+    }
+}
